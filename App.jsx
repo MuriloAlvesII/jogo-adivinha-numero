d696f90269ce02f871e3f909cbc69c8befe8d621
pedroemanuel025 <emanuel.n@escolar.ifrn.edu.br>
2026-08-14 23:24:00 -0300

    Add files via upload
    Pequenas atualizações feitas na parte do front

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import './styles/index.css'
+
+function App() {
+
+
+
+
+  return (
+    <div>
+      <header>
+        <h1 className="titulo">Jogo de Adivinhação</h1>
+      </header>
+
+    <div className='div-botao'>
+       <button><a href="jogo">Jogar</a></button>
+    </div>
+
+    </div>
+
+)
+
+}
+
+export default App
